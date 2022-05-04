eb10f124086132a1cccdea7eeb1127fd3e11b9f9
gruntik <gruntov99@mail.ru>
2022-05-04 14:51:52 +0300

    ServletContext and ServletConfig

---
+++ src/main/java/com/gruntik/MyServlet.java
@@ -0,0 +1,30 @@
+package com.gruntik;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		PrintWriter pr = res.getWriter();
+
+		ServletContext ctx = getServletContext();
+		String name = ctx.getInitParameter("name");
+
+		ServletConfig cfg = getServletConfig();
+		String nameCfg = cfg.getInitParameter("name");
+
+		pr.println("hello");
+		pr.println(name);
+		pr.println(nameCfg);
+
+	}
+
+}
